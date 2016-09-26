dba69b37a320d8d5a39b6573d29501fd152d81de
Shiva Vanamala <shiva.sreenath@gmail.com>
2016-09-25 22:25:08 -0700

    store word2vec model

---
+++ src/data_helpers.py
@@ -0,0 +1,142 @@
+'''
+Created on Sep 25, 2016
+
+@author: shiva
+'''
+import numpy as np
+import re
+import itertools
+from collections import Counter
+
+"""
+Original taken from https://github.com/alexander-rakhlin/CNN-for-Sentence-Classification-in-Keras
+"""
+
+DATA_FILE_PRE = "../data/processed/Author"
+DATA_FILE_POST = ".txt"
+
+NUM_AUTHORS = 2
+PARAGRAPH_SIZE = 30
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def load_data_and_labels():
+    """
+    Loads MR polarity data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    x = []
+    num_examples = []
+    
+    for i in range(1, NUM_AUTHORS+1):
+        author_examples = []
+        
+        author_lines = [clean_str(line).split(" ") for line in open(DATA_FILE_PRE + str(i) + DATA_FILE_POST).readlines()]
+        
+        author_paragraphs = [author_lines[j:j+PARAGRAPH_SIZE] for j in range(0, len(author_lines),PARAGRAPH_SIZE)]
+        author_paragraphs = [sum(lines, []) for lines in author_paragraphs]        
+        
+        x.extend(author_paragraphs)
+        num_examples.append(len(author_paragraphs))
+        
+    # set labels
+    y = []
+    labels = np.eye(NUM_AUTHORS)
+    
+    for i in range(NUM_AUTHORS):
+        author_labels = [labels[i] for _ in range(num_examples[i])]
+        
+        y.extend(author_labels)
+    
+    return [x, y]
+
+
+def pad_sentences(sentences, padding_word="<PAD/>"):
+    """
+    Pads all sentences to the same length. The length is defined by the longest sentence.
+    Returns padded sentences.
+    """
+    sequence_length = max(len(x) for x in sentences)
+    padded_sentences = []
+    for i in range(len(sentences)):
+        sentence = sentences[i]
+        num_padding = sequence_length - len(sentence)
+        new_sentence = sentence + [padding_word] * num_padding
+        padded_sentences.append(new_sentence)
+    return padded_sentences
+
+
+def build_vocab(sentences):
+    """
+    Builds a vocabulary mapping from word to index based on the sentences.
+    Returns vocabulary mapping and inverse vocabulary mapping.
+    """
+    # Build vocabulary
+    word_counts = Counter(itertools.chain(*sentences))
+    # Mapping from index to word
+    vocabulary_inv = [x[0] for x in word_counts.most_common()]
+    # Mapping from word to index
+    vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
+    return [vocabulary, vocabulary_inv]
+
+
+def build_input_data(sentences, labels, vocabulary):
+    """
+    Maps sentencs and labels to vectors based on a vocabulary.
+    """
+    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
+    y = np.array(labels)
+    return [x, y]
+
+
+def load_data():
+    """
+    Loads and preprocessed data for the MR dataset.
+    Returns input vectors, labels, vocabulary, and inverse vocabulary.
+    """
+    # Load and preprocess data
+    sentences, labels = load_data_and_labels()
+    sentences_padded = pad_sentences(sentences)
+    vocabulary, vocabulary_inv = build_vocab(sentences_padded)
+    x, y = build_input_data(sentences_padded, labels, vocabulary)
+    return [x, y, vocabulary, vocabulary_inv]
+
+def data_split():
+    pass
+    return [x_train, y_train, x_val, y_val, x_test, y_test]
+
+def batch_iter(data, batch_size, num_epochs):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_data = data[shuffle_indices]
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
